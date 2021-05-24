fde8976f1486b3f21835c5aa5211dbe62917f977
liuarui <liuwenrui597@qq.com>
2021-05-24 22:10:44 +0800

    chore: 项目初始化

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+import XCenter from './x坐标label效果实现/index'
+
+export default function app() {
+  return (
+    <Fragment>
+      <XCenter />
+    </Fragment>
+  )
+}
---
+++ src/x坐标label效果实现/index.jsx
@@ -0,0 +1,79 @@
+import * as echarts from 'echarts'
+import ReactECharts from 'echarts-for-react'
+import dayjs from 'dayjs'
+
+// 生成数据
+let data = []
+const oneDay = 1000 * 60 * 60 * 24
+let baseTime = new Date().setHours(0, 0, 0, 0)
+for (let i = 0; i < 30; i++) {
+    data.push([baseTime - i * oneDay, Math.ceil(i / 10)])
+}
+// 打印生成的数据 调试
+console.log(data.reverse().map(v => dayjs(v[0]).format(`YYYY-MM-DD`)))
+
+function getOption() {
+    return {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {}
+        },
+        xAxis: {
+            type: 'time',
+            minInterval: 3600 * 24 * 1000,
+            maxInterval: 3600 * 24 * 1000,
+            axisTick: {
+                show: false,
+            },
+            axisLabel: {
+                showMinLabel: true,
+                showMaxLabel: true,
+                interval: 0,
+                align: 'center',
+                formatter: (value, index) => {
+                    const formatValue = dayjs(value).format(`YYYY-MM-DD`)
+                    if (index === 0) {
+                        // 第一个 label
+                        return `{first|${formatValue}}`
+                    } else if (index === Math.ceil(data.length / 2)) {
+                        // 中间的label
+                        return `{middle|${formatValue}}`
+                    } else if (index === data.length + 1) {
+                        return `{last|${formatValue}}`
+                    }
+                    // 其他不展示
+                    return ''
+                },
+                rich: {
+                    first: {
+                        align: 'left',
+                        color: 'red',
+                        width: 1
+                    },
+                    middle: {
+                        align: 'center'
+                    },
+                    last: {
+                        width: 1,
+                        color: 'red',
+                        align: 'right'
+                    }
+                }
+            },
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: data.reverse(),
+            type: 'line',
+            smooth: true
+        }]
+    }
+}
+function App() {
+    return <ReactECharts echarts={echarts} option={getOption()} />
+
+}
+
+export default App
